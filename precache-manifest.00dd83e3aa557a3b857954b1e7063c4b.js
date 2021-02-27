ea337e487b03a6f8325ca4a89bfe2363978409b8
Victor Lin <linvictorw@gmail.com>
2021-02-27 17:17:22 +0800

    Updates

---
+++ precache-manifest.00dd83e3aa557a3b857954b1e7063c4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da4e62fd4dad154b65e34ca818aec61",
+    "url": "/translate/index.html"
+  },
+  {
+    "revision": "f9f40729c43ed67cb338",
+    "url": "/translate/static/js/2.0a887064.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/translate/static/js/2.0a887064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f0c9e135550660d954",
+    "url": "/translate/static/js/main.8eb3b488.chunk.js"
+  },
+  {
+    "revision": "d0e1e6c0636c11f91ff2",
+    "url": "/translate/static/js/runtime-main.25985187.js"
+  }
+]);
